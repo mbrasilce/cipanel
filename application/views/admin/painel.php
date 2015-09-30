3e5d39d9b8de0068ae3cbdeb4837668cf0f94387
Márcio Brasil <marcio@mbrasil.net>
2015-09-30 10:32:55 -0300

    Primeiro Commit

---
+++ application/views/admin/painel.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Painel</title>
+    </head>
+    <body>
+        <h1>Painel</h1>
+        <a href="<?php echo site_url(); ?>auth/logout">Sair</a>
+
+    </body>
+</html>
+
